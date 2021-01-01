5e36699fd7285c76150bc806256e7ab4e23994cf
avigael <code@gaels.us>
2021-01-01 02:02:56 -0600

    random fix

--- accounts.py
+++ accounts.py
@@ -4,11 +4,10 @@ from random import randint
 
 with open('accounts.json') as acc_data:
     data = json.load(acc_data)
-SIZE = len(data)
 
 def get_accounts(amount):
     accounts = []
     for i in range(0, amount):
-        acc = randint(1, SIZE)
+        acc = randint(1, 8)
         accounts.append(data[acc])
     return accounts
